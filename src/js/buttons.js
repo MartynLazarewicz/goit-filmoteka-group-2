e86d45d824686ea8a69a9ca5fbca6831edd3f696
Paweł Kłodowski <angryface97@gmail.com>
2022-07-18 16:22:29 +0200

    Queue
    -Filmy dodają się do kolejki
    -Działają przyciski watched i queue

---
+++ src/js/buttons.js
@@ -0,0 +1,15 @@
+const btnWatched = document.getElementById('btn-watched')
+const btnQueue = document.getElementById('btn-queue')
+
+function eventWatched() {
+    document.getElementById('main-watched').style.display = "flex";
+    document.getElementById('main-queue').style.display = "none";
+}
+
+function eventQueue() {
+    document.getElementById('main-watched').style.display = "none";
+    document.getElementById('main-queue').style.display = "flex";
+}
+
+btnWatched.onclick = eventWatched;
+btnQueue.onclick = eventQueue;
--- src/js/main-library.js
+++ src/js/main-library.js
@@ -1,8 +1,10 @@
 import { getMovieBoxView, getMovieModal } from './movie-view-generate.js';
 import { genre } from './genres.js';
 const mainWatched = document.querySelector('#main-watched');
+const mainQueue = document.querySelector('#main-queue');
 
 const libraryWatched = JSON.parse(localStorage.getItem('libraryWatched'));
+const libraryQueue = JSON.parse(localStorage.getItem('libraryQueue'));
 
 const getGenreNamesFromIds = function (genre_ids) {
   let genreArrayOfObj = genre.filter(function (g) {
@@ -40,6 +42,45 @@ libraryWatched.forEach(movie => {
 
   movieEl.innerHTML = movieBox + modal;
   mainWatched.appendChild(movieEl);
+
+  document.getElementById(movie.id).addEventListener('click', () => {
+    document.getElementById('modal' + movie.id).style.display = 'block';
+    document.querySelector('body').style.overflow = 'hidden';
+  });
+
+  document.getElementById('close' + movie.id).addEventListener('click', () => {
+    document.getElementById('modal' + movie.id).style.display = 'none';
+    document.querySelector('body').style.overflow = 'visible';
+  });
+});
+
+libraryQueue.forEach(movie => {
+  const movieEl = document.createElement('div');
+  movieEl.classList.add('movie');
+  const genresNames = getGenreNamesFromIds(movie.genre_ids);
+
+  const movieBox = getMovieBoxView(
+    movie.id,
+    movie.poster_path,
+    movie.title,
+    movie.vote_average,
+    genresNames,
+    movie.release_date
+  );
+  const modal = getMovieModal(
+    movie.id,
+    movie.poster_path,
+    movie.title,
+    movie.vote_average,
+    movie.vote_count,
+    movie.popularity,
+    movie.original_title,
+    genresNames,
+    movie.overview
+  );
+
+  movieEl.innerHTML = movieBox + modal;
+  mainQueue.appendChild(movieEl);
 
   document.getElementById(movie.id).addEventListener('click', () => {
     document.getElementById('modal' + movie.id).style.display = 'block';
